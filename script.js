c2b934cdf6e0a4e8ec500c99e208108ebb865322
Nico <disconico@outlook.com>
2022-05-16 19:58:37 +0200

    Add working script for the calculator

--- script.js
+++ script.js
@@ -4,47 +4,78 @@ subtract = (a, b) => a -= b
 multiply = (a, b) => a *= b
 divide = (a, b) => a /= b
 power = (a, b) => Math.pow(a, b)
-operate = (operator, a, b) => {
-    console.log(typeof(operator));
-    return operator(a, b);
-}
+operate = (operator, a, b) => {return operator(a, b)};
 
-console.log(operate(add,2,3));
-
+//Assign all elements
 const nbrBtn = document.querySelectorAll('.number-button')
 const opeBtn = document.querySelectorAll('.operator-button')
 const totalBtn = document.getElementById('total')
+const returnBtn = document.getElementById('return')
 const clearBtn = document.getElementById('clear')
 const display = document.getElementById('display')
 let storage = []
-let a ;
-let b ;
+let a = 0;
+let b = 0;
 let ope ;
 
 nbrBtn.forEach(button => button.addEventListener('click', (e)=> {
     let input = e.target.id;
     display.innerText = `${storage.join('')}${input}`;
     storage.push(input);
-    return storage;
+    console.log(storage);
 }))
 
 opeBtn.forEach(button => button.addEventListener('click', (e) => {
-    a = parseInt(storage.join(''));
+    a = parseFloat(storage.join(''));
     storage = []
-    display.innerText = '';
     ope = e.target.id;
-    return a, ope; 
 }))
 
 totalBtn.addEventListener('click', () => {
     b = parseInt(storage.join(''));
-    console.log(ope);
-    console.log(a);
-    console.log(typeof(a));
-    console.log(b);
-    console.log(typeof(b));
-    result = operate(ope, a, b)
-    display.innerText = `${result}`
+    let result = 0;
+    if (ope === 'add') {
+        result = operate(add, a, b);
+        storage = [result];
+        if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+        display.innerText = roundedResult;
+        b = 0;
+    } else if (ope === 'subtract') {
+        result = operate(subtract, a, b);
+        storage = [result];
+        if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+        display.innerText = roundedResult;
+        b = 0;
+    } else if (ope === 'multiply') {
+        result = operate(multiply, a, b);
+        storage = [result];
+        if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+        display.innerText = roundedResult;
+        b = 0;
+    } else if (ope === 'divide') {
+        if (b === 0) {
+            display.innerText = 'BOOM I EXPLODED'
+        } else {
+            result = operate(divide, a, b);
+            storage = [result];
+            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1))
+                display.innerText = roundedResult;
+            b = 0;
+        }
+    } else {
+        result = 0;
+        storage = [result];
+        display.innerText = result;
+        b = 0;
+    }
+})
+
+returnBtn.addEventListener('click', () => {
+    storage = Array.from(display.innerText = display.innerText
+        .toString()
+        .slice(0, -1))
+    console.log(storage);    
+    return storage;    
 })
 
 clearBtn.addEventListener('click', () => {
@@ -52,5 +83,6 @@ clearBtn.addEventListener('click', () => {
     a = 0;
     b = 0;
     result = 0;
-    return storage, a, b, result;
+    display.innerText = 0
 });
+
