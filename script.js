b4a43d9883119f06d0556c7a8023bef51947776d
Nico <disconico@outlook.com>
2022-05-17 17:04:12 +0200

    Add multi operations functionnality

--- script.js
+++ script.js
@@ -14,46 +14,78 @@ const returnBtn = document.getElementById('return')
 const clearBtn = document.getElementById('clear')
 const display = document.getElementById('display')
 let storage = []
+let result = 0;
 let a = 0;
 let b = 0;
 let ope;
 
-nbrBtn.forEach(button => button.addEventListener('click', (e) => {
+//Buttons events
+nbrBtn.forEach(button => button.addEventListener('click', numberPressed))
+opeBtn.forEach(button => button.addEventListener('click', operatorPressed))
+totalBtn.addEventListener('click', calculate)
+returnBtn.addEventListener('click', returnPressed)
+clearBtn.addEventListener('click', clearAll)
+
+//Mains functions
+function numberPressed (e) {
     if (storage.length <= 10) {
         let input = e.target.id;
         display.innerText = `${storage.join('')}${input}`;
         storage.push(input);
-        console.log(storage);
     }
-}))
+}
 
-opeBtn.forEach(button => button.addEventListener('click', (e) => {
-    a = parseFloat(storage.join(''));
-    storage = []
-    ope = e.target.id;
-}))
+function operatorPressed (e) {
+    if (a === 0) {
+        a = parseFloat(storage.join(''));
+        storage = []
+        ope = e.target.id;
+    } else {
+        calculate ();
+        a = parseFloat(storage.join(''));
+        storage = []
+        ope = e.target.id;
+    }
+}
 
-totalBtn.addEventListener('click', () => {
-    b = parseInt(storage.join(''));
-    let result = 0;
+function returnPressed () {
+    storage = Array.from(display.innerText = display.innerText
+        .toString()
+        .slice(0, -1))
+    return storage;
+}
+
+function clearAll () {
+    storage = [];
+    a = 0;
+    b = 0;
+    result = 0;
+    display.innerText = 0
+}
+
+function calculate() {
+    b = parseFloat(storage.join(''));
     if (ope === 'add') {
         result = operate(add, a, b);
         storage = [result];
         if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
         display.innerText = roundedResult;
         b = 0;
+        a = 0;
     } else if (ope === 'subtract') {
         result = operate(subtract, a, b);
         storage = [result];
         if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
         display.innerText = roundedResult;
         b = 0;
+        a = 0;
     } else if (ope === 'multiply') {
         result = operate(multiply, a, b);
         storage = [result];
         if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
         display.innerText = roundedResult;
         b = 0;
+        a = 0;
     } else if (ope === 'divide') {
         if (b === 0) {
             display.innerText = 'BOOM I EXPLODED'
@@ -63,30 +95,16 @@ totalBtn.addEventListener('click', () => {
             if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1))
                 display.innerText = roundedResult;
             b = 0;
+            a = 0;
         }
     } else {
         result = 0;
         storage = [result];
         display.innerText = result;
         b = 0;
+        a = 0;
     }
-})
-
-returnBtn.addEventListener('click', () => {
-    storage = Array.from(display.innerText = display.innerText
-        .toString()
-        .slice(0, -1))
-    console.log(storage);
-    return storage;
-})
-
-clearBtn.addEventListener('click', () => {
-    storage = [];
-    a = 0;
-    b = 0;
-    result = 0;
-    display.innerText = 0
-});
+}
 
 // Add keyboard support
 window.addEventListener('keydown', keyboardInput);
@@ -106,47 +124,18 @@ function keyboardInput(e) {
         result = 0;
         display.innerText = 0
     } else if (e.key === '=' || e.key === 'Enter') {
-        b = parseInt(storage.join(''));
-        let result = 0;
-        if (ope === 'add') {
-            result = operate(add, a, b);
-            storage = [result];
-            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
-            display.innerText = roundedResult;
-            b = 0;
-        } else if (ope === 'subtract') {
-            result = operate(subtract, a, b);
-            storage = [result];
-            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
-            display.innerText = roundedResult;
-            b = 0;
-        } else if (ope === 'multiply') {
-            result = operate(multiply, a, b);
-            storage = [result];
-            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
-            display.innerText = roundedResult;
-            b = 0;
-        } else if (ope === 'divide') {
-            if (b === 0) {
-                display.innerText = 'BOOM I EXPLODED'
-            } else {
-                result = operate(divide, a, b);
-                storage = [result];
-                if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1))
-                    display.innerText = roundedResult;
-                b = 0;
-            }
-        } else {
-            result = 0;
-            storage = [result];
-            display.innerText = result;
-            b = 0;
-        }
+        calculate();
     } else if (e.key === '-' || e.key === '+' || e.key === '*' || e.key === '%') {
+        if (a === 0) {
         a = parseFloat(storage.join(''));
         storage = []
         ope = convertOperator(e.key);
-        console.log(ope)
+        } else {
+            calculate();
+            a = parseFloat(storage.join(''));
+            storage = []
+            ope = convertOperator(e.key);
+        }
     } else if (e.key === 'Backspace') {
         storage = Array.from(display.innerText = display.innerText
             .toString()
