ff28f877d752e408207b2356010a927a67e845ae
Nico <disconico@outlook.com>
2022-05-17 11:27:55 +0200

    Add working version

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ subtract = (a, b) => a -= b
 multiply = (a, b) => a *= b
 divide = (a, b) => a /= b
 power = (a, b) => Math.pow(a, b)
-operate = (operator, a, b) => {return operator(a, b)};
+operate = (operator, a, b) => { return operator(a, b) };
 
 //Assign all elements
 const nbrBtn = document.querySelectorAll('.number-button')
@@ -16,9 +16,9 @@ const display = document.getElementById('display')
 let storage = []
 let a = 0;
 let b = 0;
-let ope ;
+let ope;
 
-nbrBtn.forEach(button => button.addEventListener('click', (e)=> {
+nbrBtn.forEach(button => button.addEventListener('click', (e) => {
     let input = e.target.id;
     display.innerText = `${storage.join('')}${input}`;
     storage.push(input);
@@ -74,8 +74,8 @@ returnBtn.addEventListener('click', () => {
     storage = Array.from(display.innerText = display.innerText
         .toString()
         .slice(0, -1))
-    console.log(storage);    
-    return storage;    
+    console.log(storage);
+    return storage;
 })
 
 clearBtn.addEventListener('click', () => {
@@ -86,3 +86,77 @@ clearBtn.addEventListener('click', () => {
     display.innerText = 0
 });
 
+// Add keyboard support
+window.addEventListener('keydown', keyboardInput);
+
+function keyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9 || e.key === '.') {
+        let input = e.key;
+        display.innerText = `${storage.join('')}${input}`;
+        storage.push(input);
+        console.log(storage);
+    } else if (e.key === 'Escape') {
+        storage = [];
+        a = 0;
+        b = 0;
+        result = 0;
+        display.innerText = 0
+    } else if (e.key === '=' || e.key === 'Enter') {
+        b = parseInt(storage.join(''));
+        let result = 0;
+        if (ope === 'add') {
+            result = operate(add, a, b);
+            storage = [result];
+            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+            display.innerText = roundedResult;
+            b = 0;
+        } else if (ope === 'subtract') {
+            result = operate(subtract, a, b);
+            storage = [result];
+            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+            display.innerText = roundedResult;
+            b = 0;
+        } else if (ope === 'multiply') {
+            result = operate(multiply, a, b);
+            storage = [result];
+            if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1));
+            display.innerText = roundedResult;
+            b = 0;
+        } else if (ope === 'divide') {
+            if (b === 0) {
+                display.innerText = 'BOOM I EXPLODED'
+            } else {
+                result = operate(divide, a, b);
+                storage = [result];
+                if (Number.isInteger(result) ? roundedResult = result : roundedResult = result.toFixed(1))
+                    display.innerText = roundedResult;
+                b = 0;
+            }
+        } else {
+            result = 0;
+            storage = [result];
+            display.innerText = result;
+            b = 0;
+        }
+    } else if (e.key === '-' || e.key === '+' || e.key === '*' || e.key === '%') {
+        a = parseFloat(storage.join(''));
+        storage = []
+        ope = convertOperator(e.key);
+        console.log(ope)
+    } else if (e.key === 'Backspace') {
+        storage = Array.from(display.innerText = display.innerText
+            .toString()
+            .slice(0, -1))
+        console.log(storage);
+        return storage;
+    } else {
+        return false;
+    }
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return 'divide'
+    if (keyboardOperator === '*') return 'multiply'
+    if (keyboardOperator === '-') return 'subtract'
+    if (keyboardOperator === '+') return 'add'
+}
